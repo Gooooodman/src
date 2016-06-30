b766b83de9769c0b6dd8715e8dfb408c25e8aa49
鲁普啸 <694366594@qq.com>
2016-06-30 14:13:02 +0800

    map  re
    生成字典

--- log_split/test.go
+++ log_split/test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -10,34 +10,73 @@ import (
 	"strings"
 )
 
-func ReadLine(fileName string, handler func(string)) error {
+func ReadLine(fileName string) (error, map[int]map[string]string) {
 	f, err := os.Open(fileName)
 	if err != nil {
-		return err
+		return err, nil
 	}
 	buf := bufio.NewReader(f)
+
+	Binfo := make(map[int]map[string]string)
+	num := 0
 	for {
 		line, err := buf.ReadString('\n')
-		line = strings.TrimSpace(line)
-		handler(line)
 		if err != nil {
+			//读完了之后 break
 			if err == io.EOF {
-				return nil
+				//return nil, Binfo
+				break
 			}
-			return err
+			return err, nil
 
 		}
+		line = strings.TrimSpace(line)
+		//handler(line)
+		r, _ := regexp.Compile("T\\d{2}:\\d{2}:\\d{2}.*")
+		o := r.ReplaceAllString(line, "")
+		r, _ = regexp.Compile("[[:space:]]")
+		//fmt.Println(strings.Replace(r.ReplaceAllString(o, "&localtime="), "http://sdklog.tj.65.com/1.html?", "", 1))
+		S_line := strings.Replace(r.ReplaceAllString(o, "&localtime="), "http://sdklog.tj.65.com/1.html?", "", 1)
+		sp_one := strings.Split(S_line, "&")
+
+		info, ok := Binfo[num]
+		if !ok {
+			info = make(map[string]string)
+			Binfo[num] = info
+			for _, v := range sp_one {
+				v1 := strings.Split(v, "=")
+				//fmt.Println(v1[1])
+				info[v1[0]] = v1[1]
+			}
+			num++
+		}
+
 	}
-	return nil
+	return nil, Binfo
 }
 
 func main() {
 
-	ReadLine("test2.log", func(out string) {
-		r, _ := regexp.Compile("T\\d{2}:\\d{2}:\\d{2}.*")
-		o := r.ReplaceAllString(out, "")
-		r, _ = regexp.Compile("[[:space:]]")
-		fmt.Println(strings.Replace(r.ReplaceAllString(o, "&localtime="), "http://sdklog.tj.65.com/1.html?", "", 1))
-	})
+	// ReadLine("test2.log", func(out string) {
+	// 	r, _ := regexp.Compile("T\\d{2}:\\d{2}:\\d{2}.*")
+	// 	o := r.ReplaceAllString(out, "")
+	// 	r, _ = regexp.Compile("[[:space:]]")
 
+	// 	fmt.Println(strings.Replace(r.ReplaceAllString(o, "&localtime="), "http://sdklog.tj.65.com/1.html?", "", 1))
+	// 	line := strings.Replace(r.ReplaceAllString(o, "&localtime="), "http://sdklog.tj.65.com/1.html?", "", 1)
+	// 	sp_one := strings.Split(line, "&")
+	// 	for _, v := range sp_one {
+	// 		v1 := strings.Split(v, "=")
+	// 		//fmt.Println(v1[1])
+	// 		info[v1[0]] = v1[1]
+	// 	}
+	// 	Binfo[num] = info
+	// 	num++
+	// 	fmt.Println(Binfo)
+	// })
+
+	e, B := ReadLine("test2.log")
+	if e == nil {
+		fmt.Println(B)
+	}
 }
