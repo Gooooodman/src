e3d8bbbc640cde61e319d4351686e9d64cdcc8bf
Goooood <694366594@qq.com>
2016-09-12 19:45:23 +0800

    单独一个协程作业
    go JDT

--- base/jindutiao2.go
+++ base/jindutiao2.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"time"
 )
 
 func main() {
+	ch := make(chan int)
+	ret := make(chan bool)
+	go JDT(ch, ret)
+	pinger(ch, ret)
+	fmt.Printf("\rover...............................................................................")
+}
+
+func JDT(ch chan int, ret chan bool) {
+	for {
+		select {
+		case c := <-ch:
+			h := strings.Repeat("=", c) + strings.Repeat(" ", 49-c)
+			fmt.Printf("\r[%s]%.0f%%", h, float64(c)/49*100)
+		case <-ret:
+			break
+		}
+	}
+
+}
+
+func pinger(c chan int, ret chan bool) {
 	for i := 0; i < 50; i++ {
 		time.Sleep(100 * time.Millisecond)
-		h := strings.Repeat("=", i) + strings.Repeat(" ", 49-i)
-		fmt.Printf("\r[%s]%.0f%%", h, float64(i)/49*100)
-		os.Stdout.Sync()
+		c <- i
 	}
-	fmt.Println("\nAll System Go!")
+	ret <- true
 }
+
+/*    最初
+func main() {
+    for i := 0; i < 50; i++ {
+        time.Sleep(100 * time.Millisecond)
+        h := strings.Repeat("=", i) + strings.Repeat(" ", 49-i)
+        fmt.Printf("\r[%s]%.0f%%", h, float64(i)/49*100)
+        os.Stdout.Sync()
+    }
+    fmt.Println("\nAll System Go!")
+}
+*/
