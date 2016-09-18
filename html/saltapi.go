ca72a7b098b5720a30ce684494117a8c1dc3eecd
Goooood <694366594@qq.com>
2016-09-18 23:04:55 +0800

    将ssl 忽略认证简化
    func Client() *http.Client

--- html/saltapi.go
+++ html/saltapi.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"crypto/tls"
-	//"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,17 +18,12 @@ type Server struct {
 	Fun      string `json:"fun"`
 }
 
-//type Server1 struct {
-// 	Client string `json:"client"`
-// 	Fun    string `json:"fun"`
-// }
-
 const (
 	username = "saltapi"
 	password = "saltapi"
 	eauth    = "pam"
-	url      = "https://192.168.30.129:8888/"
-	//url = "https://139.196.231.187:8888"
+	//url      = "https://192.168.30.129:8888/"
+	url = "https://139.196.231.187:8888/"
 )
 
 type Ret struct {
@@ -49,21 +43,26 @@ var obj Obj
 
 var S Server
 
-func init() {
-	//S = &Server{Username: username, Password: password, Eauth: eauth}
-	S.Username = username
-	S.Password = password
-	S.Eauth = eauth
+// ssl  忽略认证
+func Client() *http.Client {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	http := &http.Client{Transport: tr}
+	return &http.Client{Transport: tr}
+
+}
+
+func init() {
+	S.Username = username
+	S.Password = password
+	S.Eauth = eauth
+	client := Client()
 	b, err := json.Marshal(S)
 	if err != nil {
 		fmt.Println("json err:", err)
 	}
 	body := bytes.NewBuffer([]byte(b))
-	res, err := http.Post(url+"/login", "application/json;charset=utf-8", body)
+	res, err := client.Post(url+"/login", "application/json;charset=utf-8", body)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -76,40 +75,33 @@ func init() {
 	}
 
 	json.Unmarshal([]byte(result), &obj)
-	//fmt.Println(obj)
 }
 
-func (o Obj) ListALLKey() {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-
+func (o Obj) ListALLKey() (string, error) {
+	client := Client()
 	req, err := http.NewRequest("GET", url+"/keys", nil)
 	if err != nil {
-		fmt.Println("request  -- >", err)
-		return
+		return "", err
 	}
-	req.Header.Add("X-Auth-Token", obj.Return[0].Token)
 
+	req.Header.Add("X-Auth-Token", obj.Return[0].Token)
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println("clint: -- > ", err)
-		return
+		return "", err
 	}
 	if res.StatusCode != 200 {
 		fmt.Println(res.Status)
-		return
+		return "", err
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println("read:   -- >", err)
+		return "", err
 	}
-	fmt.Println(string(body))
+	return string(body), nil
 }
 
 func main() {
-	fmt.Println(obj.Return[0].Token)
-	obj.ListALLKey()
+	ret, _ := obj.ListALLKey()
+	fmt.Println(ret)
 }
