7fe38d079251031a5923312462e99bbc4c085e87
Goooood <694366594@qq.com>
2016-11-17 11:13:07 +0800

    输出更多内容

--- base/dengbi.go
+++ base/dengbi.go
@@ -6,13 +6,17 @@ import (
 )
 
 const (
-	a = 2  // 第一位
+	a = 20 // 第一位
+	n = 8  // 连续10
 	q = 2  // 平方
-	n = 15 // 连续10
+
 )
 
 func main() {
-
-	fmt.Println(a * (1 - math.Pow(q, n)) / (1 - q))
-	fmt.Println(math.Pow(2, 16))
+	for i := 1; i <= 8; i++ {
+		fmt.Printf("%v -----> %v\n", i, a*math.Pow(2, float64(i-1)))
+	}
+	fmt.Println("-----------------------------------------")
+	fmt.Printf("%v 次总投入: %v\n", n, a*(1-math.Pow(q, n))/(1-q))
+	fmt.Printf("%d 次需投入: %v\n", n+1, a*math.Pow(2, float64(n)))
 }
